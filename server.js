405a12b8beb9126b4963c1fe1e5ac05d059801a7
Xavier San Lorenzo <sanlorenzoxavier@gmail.com>
2020-12-12 11:37:33 +0800

    init

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express"); // to manipulate web server
-const mongoose = require("mongoose"); // to have a control in the database
-
+const connectDB = require("./config/db");
 const app = express();
 
+connectDB(); // connect database
 app.get("/", (req, res) => {
   res.send("Connected in port");
 });
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+//Defines Routes
+app.use("/api/users", require("./routes/api/users")); // the params is: /api/users
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 //We need to add the port and listen to it so it can connect to the URL PORT
 const PORT = process.env.PORT || 3000;
